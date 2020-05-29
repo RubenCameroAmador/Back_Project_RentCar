1fd42d070771244e7834763e23595d84d68680ce
RubenCameroAmador <rdcamero@uninorte.edu.co>
2020-05-29 12:32:36 -0500

    back terminado

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.use(express.json());
 
 /* routes */
 app.use('/users',require('./src/routes/users'))
+app.use('/cars',require('./src/routes/cars'))
+app.use('/bills',require('./src/routes/bills'))
+app.use('/places',require('./src/routes/places'))
+app.use('/reservas',require('./src/routes/reservas'))
 module.exports=app;
